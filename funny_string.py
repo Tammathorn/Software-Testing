7f272882bd9c60783962b4e06358864d714d4e2a
Tammathorn Kananurak <Tamaton01@gmail.com>
2024-02-11 22:41:47 +0700

    funny_string

---
+++ funny_string.py
@@ -0,0 +1,20 @@
+#funny string
+def funnyString(s):
+    r = s[::-1]
+    for i in range(1, len(s)):
+        if abs(ord(s[i]) - ord(s[i-1])) != abs(ord(r[i]) - ord(r[i-1])):
+            return 'Not Funny'
+    return 'Funny'
+
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = funnyString(s)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
